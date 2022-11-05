eafaf4a047d7fd5731355a484fcb09650ddce5af
Yaroslav <yaroslav-w@mail.ru>
2022-11-05 16:42:09 +0400

    fix: Commit.

--- src/main/java/org/yar/Controller.java
+++ src/main/java/org/yar/Controller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
   @GetMapping
   public String home() {
-    return "Hello!";
+    return "Hello World!";
   }
 }
